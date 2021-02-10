fd49cbfe8f3dd7e8b7f474bbf72eab842fde21ab
Mario Rezende <mariorez@gmail.com>
2021-02-10 07:01:16 -0300

    minor improvements

--- core/src/main/java/org/seariver/yokid/YokidGame.java
+++ core/src/main/java/org/seariver/yokid/YokidGame.java
@@ -1,15 +1,33 @@
 package org.seariver.yokid;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import org.seariver.yokid.screen.OpenScreen;
 
 public class YokidGame extends Game {
 
-    public static int WIDTH = 1280;
-    public static int HEIGHT = 720;
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
+    private SpriteBatch batch;
 
     @Override
     public void create() {
+        batch = new SpriteBatch();
         setScreen(new OpenScreen(this));
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        batch.dispose();
+    }
+
+    public SpriteBatch getBatch() {
+        return batch;
     }
 }
--- core/src/main/java/org/seariver/yokid/screen/MainScreen.java
+++ core/src/main/java/org/seariver/yokid/screen/MainScreen.java
@@ -1,77 +1,66 @@
 package org.seariver.yokid.screen;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import org.seariver.yokid.YokidGame;
 
 public class MainScreen implements Screen {
 
-    private Game game;
+    private final YokidGame game;
     private OrthographicCamera camera;
-    private SpriteBatch batch;
     private Texture yokidStand;
 
-    public MainScreen(Game game) {
+    public MainScreen(final YokidGame game) {
         this.game = game;
         camera = new OrthographicCamera();
-        batch = new SpriteBatch();
-    }
-
-    @Override
-    public void show() {
         camera.setToOrtho(false, YokidGame.WIDTH, YokidGame.HEIGHT);
         yokidStand = new Texture(Gdx.files.internal("yokid-front.png"));
     }
 
     @Override
-    public void render(float delta) {
+    public void show() {
+    }
 
-        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
-            game.setScreen(new OpenScreen(game));
-            return;
-        }
+    @Override
+    public void render(float delta) {
 
         // clear screen
         ScreenUtils.clear(0, 0, 0.2f, 1);
         camera.update();
         // draw graphics
-        batch.setProjectionMatrix(camera.combined);
-        batch.begin();
-        batch.draw(yokidStand, 0, 0);
-        batch.end();
+        game.getBatch().setProjectionMatrix(camera.combined);
+        game.getBatch().begin();
+        game.getBatch().draw(yokidStand, 0, 0);
+        game.getBatch().end();
+
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
+            game.setScreen(new OpenScreen(game));
+        }
     }
 
     @Override
     public void resize(int width, int height) {
-
     }
 
     @Override
     public void pause() {
-
     }
 
     @Override
     public void resume() {
-
     }
 
     @Override
     public void hide() {
-        dispose();
     }
 
     @Override
     public void dispose() {
-        game = null;
-        camera = null;
-        batch = null;
-        yokidStand = null;
+        yokidStand.dispose();
+        game.getBatch().dispose();
     }
 }
