69c4a6750d0adab1c52f338a0cf8e2059522d636
Mario Rezende <mariorez@gmail.com>
2021-02-11 08:37:14 -0300

    implements yokid movements

--- core/src/main/java/org/seariver/yokid/actor/YokidActor.java
+++ core/src/main/java/org/seariver/yokid/actor/YokidActor.java
@@ -1,5 +1,7 @@
 package org.seariver.yokid.actor;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class YokidActor extends BaseActor {
@@ -7,5 +9,20 @@ public class YokidActor extends BaseActor {
     public YokidActor(float x, float y, Stage stage) {
         super(x, y, stage);
         setImage("yokid-front.png");
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            setImage("yokid-left.png");
+            setX(getX() - 2);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            setImage("yokid-right.png");
+            setX(getX() + 2);
+        } else {
+            setImage("yokid-front.png");
+        }
     }
 }
